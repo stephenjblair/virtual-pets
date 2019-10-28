fc66327e907e4083fde96fa1b9e77bf8f1e1879a
Stephen Blair <stephenjohnblair@outlook.com>
2019-10-28 20:30:39 +0000

    Added growUp method on Pet's prototype and passed tests

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -9,6 +9,18 @@ describe('constructor', () => {
     
         expect(pet.name).toEqual('Fido');
       });
+    test('has a initial age of 0', () => {
+        const pet = new Pet('Fido');
+    
+        expect(pet.age).toEqual(0);
+      });
+    test('increments the age by 1', () => {
+        const pet = new Pet('Fido');
+    
+        pet.growUp();
+    
+        expect(pet.age).toEqual(1);
+      });
     });
 
 /*describe("pet", () => {
--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,13 @@
 function Pet(name) {
     this.name = name;
+    this.age = 0;
+    this.growUp = function() {
+        this.age += 1;
+    };
+
+Pet.prototype.growUp = function() {
+    this.age += 1;
+    };
 }
 
 const pet = new Pet('Fido');
